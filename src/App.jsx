c849b4b70ca2d5c19f026f7a756e9ca3bbb4f49a
Michelle Perez <michelle.perez@bldr.com>
2024-04-24 04:05:24 -0400

    stable version render all dates correctly

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,45 @@
+/* eslint-disable react/prop-types */
 import { useEffect, useState, useRef } from 'react';
 import timelineItems from './timelineItems';
 
+const monthNames = {
+  1: 'January',
+  2: 'February',
+  3: 'March',
+  4: 'April',
+  5: 'May',
+  6: 'June',
+  7: 'July',
+  8: 'August',
+  9: 'September',
+  10: 'October',
+  11: 'November',
+  12: 'December',
+};
+
 function App() {
-  const dragItem = useRef();
+  // Sort events by start date, then by end date
+  const [events] = useState(
+    timelineItems.sort(
+      (a, b) => a.start.localeCompare(b.start) || a.end.localeCompare(b.end)
+    )
+  );
   const dragItemNode = useRef();
+
   const [timeLines, setTimeLines] = useState([]);
 
-  function placeEvents(events) {
-    // Sort events by start date, then by end date
-    events.sort(
-      (a, b) => a.start.localeCompare(b.start) || a.end.localeCompare(b.end)
-    );
-
+  function placeEvents() {
+    
     const lanes = [];
     const positions = [];
     events.forEach((event) => {
       let placed = false;
+      const startMonth = String(new Date(event.start).getUTCMonth() + 1).padStart(2, '0')
+      const endMonth = String(new Date(event.end).getUTCMonth() + 1).padStart(2, '0')
 
       for (let i = 0; i < positions.length; i++) {
         // Check if the event can be placed in the current lane
-        if (event.start >= positions[i]) {
+        if (event.start >= positions[i] && startMonth === endMonth) {
           // Place event in this lane
           lanes[i].push(event);
           // Update the last used position in this lane
@@ -33,104 +53,180 @@ function App() {
       if (!placed) {
         lanes.push([event]);
         positions.push(event.end);
+
+        if(startMonth !== endMonth){
+          const endDays = String(new Date(event.end).getUTCDate()).padStart(2, '0')
+          lanes[lanes.length-1].push({
+            ...event,
+            start: `2021-${endMonth}-01`,
+            end: `2021-${endMonth}-${endDays}`,
+          });
+        }
       }
     });
 
     setTimeLines(lanes);
   }
 
-  useEffect(() => placeEvents(timelineItems), [timelineItems]);
+  useEffect(() => {
+    placeEvents(events);
+  }, [events]);
 
-  const getDays = (startDate, endDate) => {
-    const diff =
-      (new Date(endDate) - new Date(startDate)) / (1000 * 60 * 60 * 24);
-    return diff + 1;
-  };
-
-  const handleDragStart = (e, item) => {
+  function handleDragStart(e, event) {
     dragItemNode.current = e.target;
-    dragItemNode.current.addEventListener('dragend', handleDragEnd);
-    dragItem.current = item;
 
     setTimeout(() => {
       if (dragItemNode.current) {
         dragItemNode.current.style.display = 'none';
       }
     }, 0);
-  };
-
-  const handleDragEnd = () => {
-    if (dragItemNode.current) {
-      dragItemNode.current.style.display = 'block';
-      dragItemNode.current.removeEventListener('dragend', handleDragEnd);
-      dragItem.current = null;
-      dragItemNode.current = null;
-    }
-  };
-
-  const handleDrop = (e, newStartDate) => {
-    e.preventDefault();
-    const draggedEvent = timeLines.find(
-      (event) => event.id === dragItem.current.id
-    );
-    const duration =
-      new Date(draggedEvent.endDate) - new Date(draggedEvent.startDate);
-    const updatedEndDate = new Date(newStartDate.getTime() + duration);
-
-    const updatedEvents = timeLines.map((event) =>
-      event.id === draggedEvent.id
-        ? {
-            ...event,
-            startDate: newStartDate.toISOString().split('T')[0],
-            endDate: updatedEndDate.toISOString().split('T')[0],
-          }
-        : event
-    );
-
-    setTimeLines(updatedEvents);
-    handleDragEnd();
-  };
-
-  function handleDragOver(e) {
-    e.preventDefault(); // Necessary for allowing a drop
   }
 
-  return (
-    <div>
-      <h2 className="my-2">January</h2>
-      <div className="flex justify-between mb-2">
+  function handleDragEnd(e) {
+    console.log('handleDragEnd ', e);
+    if (dragItemNode.current) {
+      dragItemNode.current.style.display = 'block';
+      // dragItemNode.current.removeEventListener('dragend', handleDragEnd);
+      // dragItemNode.current = null;
+    }
+  }
+
+  const handleDrop = (e) => {
+    console.log('handleDrop: ', e.target);
+    // console.log('handleDragLeave: ', dragEvent);
+    e.target.style.backgroundColor = '';
+  };
+
+  const handleDragLeave = (e) => {
+    e.target.style.backgroundColor = '';
+  };
+
+  const handleDragEnter = (e) => {
+    console.log('handleDragEnter', e);
+    e.target.style.backgroundColor = '#3c3c41';
+  };
+
+  const getMonthData = (date) => {
+    const formatedDate = new Date(date);
+    const month = String(formatedDate.getUTCMonth() + 1).padStart(2, '0');
+    const days = new Date(formatedDate.getFullYear(), month, 0).getDate();
+    const year = formatedDate.getFullYear();
+    const name = monthNames[formatedDate.getUTCMonth() + 1]
+
+    return { month, days, year, name }
+  }
+
+  const getMonths = () => {
+    const tempEvents = [...events];
+    
+    const min = getMonthData(tempEvents[0].start);
+    min.date =tempEvents[0].start
+
+    const max = getMonthData(tempEvents[tempEvents.length-1].end);
+    max.date = tempEvents[tempEvents.length-1].end
+
+    const result = min.name === max.name ? [min] : [min, max];
+    // console.log('result: ', result);
+    return result;
+  };
+
+  const renderRow = (date, className, showDays = false) => {
+    return (
+      <div
+        onDragEnter={handleDragEnter}
+        onDragLeave={handleDragLeave}
+        className="flex justify-start"
+      >
         {' '
-          .repeat(31)
+          .repeat(date.days)
           .split('')
           .map((day, index) => (
-            <div className="w-[31px] text-center bg-gray-900 p-2" key={index}>
-              {index + 1}
+            <div
+              key={`${date.year}-${date.month}-${String(index + 1).padStart(
+                2,
+                '0'
+              )}`}
+              id={`${date.year}-${date.month}-${String(index + 1).padStart(
+                2,
+                '0'
+              )}`}
+              className={`w-[31px] h-[36px] text-center p-2 ${className}`}
+            >
+              {showDays ? index + 1 : ''}
             </div>
           ))}
       </div>
-      {timeLines.map((row, indexRow) => (
-        <div className="relative" key={indexRow}>
-          {row.map((event, eventIndex) => (
-            <span
-              draggable
-              onDragStart={(e) => handleDragStart(e, event)}
-              onDragOver={handleDragOver}
-              onDrop={(e) => handleDrop(e, new Date(event.startDate))}
-              style={{
-                top: indexRow * 50,
-                left: (961 / 31) * (new Date(event.start).getUTCDate() - 1),
-                width: (getDays(event.start, event.end) * 961) / 31,
-              }}
-              className={`bg-cyan-900 p-2 rounded absolute overflow-hidden text-ellipsis text-nowrap border-1 border-cyan-500`}
-              key={event.id}
-            >
-              {getDays(event.start, event.end)} days
-            </span>
+    );
+  };
+
+  return (
+    <div>
+      {getMonths().map((item, index) => (
+        <div key={index}>
+          <h2 className="my-2">{item.name}</h2>
+          {renderRow(item, 'bg-zinc-700', true)}
+          {timeLines.map((row, indexRow) => (
+            <div id={indexRow} className="relative" key={indexRow}>
+              {row.map((event) =>
+                monthNames[new Date(event.start).getUTCMonth() + 1] ===
+                item.name ? (
+                  <EventBar
+                    key={event.id}
+                    event={event}
+                    onDragStart={(e) => handleDragStart(e, event)}
+                    onDragEnd={handleDragEnd}
+                  />
+                ) : (
+                  ''
+                )
+              )}
+              {renderRow(item, 'border-l-2 border-zinc-800/20')}
+            </div>
           ))}
         </div>
       ))}
     </div>
   );
 }
+
+const EventBar = ({ event, eventRef, ...rest }) => {
+  const getDays = (startDate, endDate) => {
+    let diff =
+      (new Date(endDate) - new Date(startDate)) / (1000 * 60 * 60 * 24);
+
+      const startMonth = monthNames[new Date(startDate).getUTCMonth() + 1]
+      const endMonth = monthNames[new Date(endDate).getUTCMonth() + 1]
+      const endDays = new Date(endDate).getUTCDate()
+
+      if(startMonth !== endMonth){
+        diff -= endDays
+      }
+
+    return diff + 1;
+  };
+
+
+  return (
+    <div
+      ref={eventRef}
+      draggable
+      style={{
+        top: 1,
+        left: (961 / 31) * (new Date(event.start).getUTCDate() - 1),
+        width: (getDays(event.start, event.end) * 961) / 31,
+      }}
+      className={`bg-cyan-900 border-cyan-500 p-1 rounded border absolute overflow-hidden text-ellipsis text-nowrap`}
+      key={event.id}
+      {...rest}
+    >
+      {/* {new Date(event.start).getUTCDate()} */}
+      {/* {getDays(event.start, event.end)}  -{' '} */}
+      {/* {new Date(event.start).getUTCDate() - 1 + getDays(event.start, event.end)} */}
+      {/* {monthNames[new Date(event.start).getUTCMonth() + 1]} -
+      {event.start} */}
+      {event.name}
+    </div>
+  );
+};
 
 export default App;
--- src/timelineItems.js
+++ src/timelineItems.js
@@ -1,87 +1,93 @@
 const timelineItems = [
   {
     id: 1,
-    start: "2021-01-03",
-    end: "2021-01-08",
-    name: "Event 1"
+    start: "2021-01-01",
+    end: "2021-01-05",
+    name: "First item"
   },
   {
     id: 2,
-    start: "2021-01-05",
+    start: "2021-01-02",
     end: "2021-01-08",
-    name: "Event 2"
+    name: "Second item"
   },
   {
     id: 3,
     start: "2021-01-06",
     end: "2021-01-13",
-    name: "Event 3"
+    name: "Another item"
   },
   {
     id: 4,
     start: "2021-01-14",
     end: "2021-01-14",
-    name: "Event 4"
+    name: "Another item"
+  },
+  {
+    id: 5,
+    start: "2021-02-01",
+    end: "2021-02-15",
+    name: "Third item"
+  },
+  {
+    id: 6,
+    start: "2021-01-12",
+    end: "2021-02-16",
+    name: "6 item with a super long name"
+  },
+  {
+    id: 7,
+    start: "2021-02-01",
+    end: "2021-02-02",
+    name: "Fifth item with a super long name"
   },
   {
     id: 8,
     start: "2021-01-03",
     end: "2021-01-05",
-    name: "Event 8"
+    name: "First item"
   },
   {
     id: 9,
     start: "2021-01-04",
     end: "2021-01-08",
-    name: "Event 9"
+    name: "Second item"
   },
   {
     id: 10,
     start: "2021-01-06",
     end: "2021-01-13",
-    name: "Event 10"
+    name: "Another item"
   },
   {
     id: 11,
     start: "2021-01-09",
     end: "2021-01-09",
-    name: "Event 11"
+    name: "Another item"
   },
-   // {
-  //   id: 5,
-  //   start: "2021-02-01",
-  //   end: "2021-02-15",
-  //   name: "Event 5"
-  // },
+  {
+    id: 12,
+    start: "2021-02-01",
+    end: "2021-02-15",
+    name: "Third item"
+  },
+  {
+    id: 13,
+    start: "2021-01-12",
+    end: "2021-02-16",
+    name: "13 item with a super long name"
+  },
+  {
+    id: 14,
+    start: "2021-02-01",
+    end: "2021-02-02",
+    name: "Fifth item with a super long name"
+  },
   // {
-  //   id: 6,
-  //   start: "2021-01-12",
-  //   end: "2021-02-16",
-  //   name: "Event 6 with a super long name and description"
-  // },
-  // {
-  //   id: 7,
-  //   start: "2021-02-01",
-  //   end: "2021-02-02",
-  //   name: "Event 7 with a super long name and description"
-  // },
-  // {
-  //   id: 13,
-  //   start: "2021-01-12",
-  //   end: "2021-02-16",
-  //   name: "Event 13 Fourth item with a super long name"
-  // },
-  // {
-  //   id: 12,
-  //   start: "2021-02-01",
-  //   end: "2021-02-15",
-  //   name: "Event 12"
-  // },
-  // {
-  //   id: 14,
-  //   start: "2021-02-01",
-  //   end: "2021-02-02",
-  //   name: "Event 14 Fifth item with a super long name"
+  //   id: 15,
+  //   start: "2021-02-20",
+  //   end: "2021-03-010",
+  //   name: "15 item with a super long name"
   // }
 ];
 
